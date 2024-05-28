9a9ace86f72bd8c439a5e781681afce4213f0e71
Zolomed <57671955+Zolomed@users.noreply.github.com>
2024-05-28 22:17:44 +0300

    Исправил проверку введенного телефона

--- src/main/kotlin/ru/clothingstore/register/RegisterController.kt
+++ src/main/kotlin/ru/clothingstore/register/RegisterController.kt
@@ -15,40 +15,41 @@ import java.util.*
 class RegisterController(private val call: ApplicationCall) {
 
     suspend fun registerNewUser() {
-        val registerReceiveRemote  = call.receive<RegisterReceiveRemote>()
-        if(!isValidPhone(registerReceiveRemote.phone)){
+        val registerReceiveRemote = call.receive<RegisterReceiveRemote>()
+        if (!isValidPhone(registerReceiveRemote.phone)) {
             call.respond(HttpStatusCode.BadRequest, "Phone is not valid")
-        }
-
-        val userDTO = Users.fetchUser(registerReceiveRemote.phone)
-
-        if (userDTO != null) {
-            call.respond(HttpStatusCode.Conflict, "User already exists")
         } else {
-            val token = UUID.randomUUID().toString()
 
-            try {
-                Users.insert(
-                    UserDTO(
+            val userDTO = Users.fetchUser(registerReceiveRemote.phone)
+
+            if (userDTO != null) {
+                call.respond(HttpStatusCode.Conflict, "User already exists")
+            } else {
+                val token = UUID.randomUUID().toString()
+
+                try {
+                    Users.insert(
+                        UserDTO(
+                            phone = registerReceiveRemote.phone,
+                            password = registerReceiveRemote.password
+                        )
+                    )
+
+                } catch (e: ExposedSQLException) {
+                    call.respond(HttpStatusCode.Conflict, "User already exists")
+
+                } catch (e: Exception) {
+                    call.respond(HttpStatusCode.BadRequest, "User could not be created")
+                }
+
+                Tokens.insert(
+                    TokenDTO(
                         phone = registerReceiveRemote.phone,
-                        password = registerReceiveRemote.password
+                        token = token
                     )
                 )
-
-            } catch (e: ExposedSQLException) {
-                call.respond(HttpStatusCode.Conflict, "User already exists")
-
-            } catch (e: Exception) {
-                call.respond(HttpStatusCode.BadRequest, "User could not be created")
+                call.respond(RegisterResponseRemote(token = token))
             }
-
-            Tokens.insert(
-                TokenDTO(
-                    phone = registerReceiveRemote.phone,
-                    token = token
-                )
-            )
-            call.respond(RegisterResponseRemote(token = token))
         }
     }
 }
